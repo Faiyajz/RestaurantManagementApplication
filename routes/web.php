91ca0a703850f338848ac34ed96f983af694075c
Faiyajz <faiyajz007@gmail.com>
2019-01-24 12:06:09 +0600

    Slider Show in frontend from database and Frontend logo problem fixed

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('welcome');
 
 
 Route::group(['prefix'=>'admin','middleware'=>'auth','namespace'=>'Admin'],function (){
