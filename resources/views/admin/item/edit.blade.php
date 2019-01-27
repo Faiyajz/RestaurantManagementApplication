538acf44d9fdbe40b8abf07414927ee22238273b
Faiyajz <faiyajz007@gmail.com>
2019-01-27 16:13:42 +0600

    Item Crud

---
+++ resources/views/admin/item/edit.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app')
+
+@section('title','Item')
+
+@push('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css">
+@endpush
+
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    @include('layouts.partial.msg')
+                    <div class="card">
+                        <div class="card-header card-header-primary">
+                            <h4 class="card-title ">Edit Item</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-content">
+
+                                <form method="POST" action="{{route('item.update',$item->id)}}" enctype="multipart/form-data">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label class="bmd-label-floating">Category</label>
+                                                <select class="form-control" name="category">
+                                                    @foreach($categories as $category)
+                                                        <option {{$category->id==$item->category->id ? 'selected' : ''}} value="{{$category->id}}">{{$category->name}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label class="bmd-label-floating">Name</label>
+                                                <input type="text" class="form-control" value="{{$item->name}}" name="name">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label class="bmd-label-floating">Description</label>
+                                                <textarea class="form-control" name="description">{{$item->description}}</textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label class="bmd-label-floating">Price</label>
+                                                <input type="number" class="form-control" value="{{$item->price}}" name="price">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <label class="bmd-label-floating">Image</label>
+                                            <input type="file" class="form-control" name="image">
+                                        </div>
+                                    </div>
+
+                                    <a href="{{route('item.index')}}" class="btn btn-danger">Back</a>
+                                    <button type="submit" class="btn btn-primary">Update</button>
+                                </form>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@push('scripts')
+
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::group(['prefix'=>'admin','middleware'=>'auth','namespace'=>'Admin'],funct
     Route::get('dashboard','DashboardController@index')->name('admin.dashboard');
     Route::resource('slider','SliderController');
     Route::resource('category','CategoryController');
+    Route::resource('item','ItemController');
 });
 
 
