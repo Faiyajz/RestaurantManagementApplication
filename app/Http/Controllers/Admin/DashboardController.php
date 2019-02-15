a55605377ce98155a25bb66c0a5c2b602bb86174
Faiyajz <faiyajz007@gmail.com>
2019-02-15 20:11:44 +0600

    Woriking with Dashboard show the not confirmed reservation in dashboard and count all the process

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Category;
+use App\Contact;
+use App\Item;
+use App\Reservation;
+use App\Slider;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('admin.dashboard');
+
+        $category_count = Category::count();
+        $item_count = Item::count();
+        $slider_count = Slider::count();
+        $reservations = Reservation::where('status',false)->get();
+        $contact_count = Contact::count();
+        return view('admin.dashboard',
+            compact('category_count','item_count','slider_count','reservations','contact_count'));
     }
 }
